5f3218b436db275db7b5bcfd8d0b0e77d959acbd
Mitchell Rosen <mitchellwrosen@gmail.com>
2013-05-24 19:42:06 -0700

    reworked card powers to better model action-reward style powers

--- Card.hs
+++ Card.hs
@@ -27,79 +27,113 @@ data NameKind = Alien
 data StartWorldColor = Red | Blue
 
 -- Powers ----------------------------------------------------------------------
+-- Explore --
 data ExplorePower =
-   -- Base
-     EDrawExtra Int            -- Draw n extra cards
-   | EKeepExtra Int            -- Keep n extra cards
-   -- RvI
-   | EMixAndMatch        -- Combine hand with draw before discard
-   -- BoW
-   | EDiscardForPrestige -- Before drawing, may discard 1 card to gain 1 prestige
+     EPDrawExtra Int -- Draw n extra cards
+   | EPKeepExtra Int -- Keep n extra cards
+   | EPMixAndMatch   -- Combine hand with draw before discard
+   | EPAction ExploreAction ExploreReward
 
+data ExploreAction =
+     EADiscard Int -- Discard n cards from hand
+
+data ExploreReward =
+     ERPrestige Int -- Gain n prestige
+
+-- Develop --
 data DevelopPower =
-     -- Base
-     DDraw Int       -- Draw n cards at the start of the development phase
-   | DReduceCost Int -- Reduce development cost by n
-   | DDrawAfter Int  -- Draw n cards after developing
+     DPDraw Int            -- Draw n cards at the start of the development phase
+   | DPDrawThenDiscard Int -- Draw n cards, then discard 1 card
+   | DPReduceCost Int      -- Reduce development cost by n
+   | DPAction DevelopAction DevelopReward
 
-   -- BOW
-   | DDrawThenDiscard Int               -- Draw n cards, then discard 1 card
-   | DPrestigeAfter CardQualifier       -- Gain 1 prestige after developing
-   | DReduceAfterSpendGood GoodKind Int -- Reduce cost by n after spending a good
-   | DSaveCardFromPayment               -- Put 1 card from a development payment (after discounts) under this world
+data DevelopAction =
+     DADevelop CardQualifier -- Develop a card of the specified kind
+   | DASpendGood GoodKind    -- Spend a good
+   | DADevelopByPayment      -- Develop a card by paying at least 1 for it after discounts
 
+data DevelopReward =
+     DRDraw Int       -- Draw n cards
+   | DRPlaceUnder     -- Place a card under this one
+   | DRPrestige Int   -- Gain n prestige
+   | DRReduceCost Int -- Reduce cost by n
+
+-- Settle --
 data SettlePower =
-     -- Base
-     SReduceCost CardQualifier Int                        -- Reduce settle cost by n
-   | SDiscardReduceCostToZero CardQualifier CardQualifier -- Discard specified card (first qualifier) to reduce settle cost of specified card (second qualifier) to 0
-   | SMilitary CardQualifier Int                          -- Gain n (specialized) military
-   | SDiscardTemporaryMilitary CardQualifier Int          -- Discard specified card from tableau to gain n military during this phase
-   | SPayForMilitary CardQualifier Int                    -- Pay for specified military world as a non-military world for n extra cost
-   | SDrawAfter Int                                       -- Draw n cards after settling
+     SPMilitary CardQualifier Int       -- Gain n (specialized) military
+   | SPPayForMilitary CardQualifier Int -- Pay for specified military world as a non-military world for n extra cost
+   | SPReduceCost CardQualifier Int     -- Reduce settle cost by n
+   | SPSettleSecondWorld                -- Settle a second world (without powers from first, do not draw a Settle bonus if the player chose Settle)
+   | SPAction SettleAction SettleReward
 
+data SettleAction =
+     SADiscardFromHandUpTo Int          -- Discard up to n cards from hand
+   | SADiscardFromTableau CardQualifier -- Discard the specified card from tableau
+   | SASettle CardQualifier             -- Settle the specified card
+
+data SettleReward =
+     SRDraw Int                       -- Draw n cards
+   | SRTemporaryMilitary Int          -- Gain n military this phase
+   | SRReduceCost CardQualifier Int   -- Reduce the specified card's cost by n
+   | SRReduceCostToZero CardQualifier -- Reduce the specified card's cost to 0
+
+-- Trade --
 data TradePower =
-     -- Base
-     TTrade CardQualifier Int -- Trade the specified card for n extra cards
+     TPAction TradeAction TradeReward
+
+data TradeAction =
+     TATrade CardQualifier -- Trade the specified card
+
+data TradeReward =
+     TADraw Int -- Draw n (extra) cards
 
 data ConsumePower =
-     -- Base
-     CConsume [CardQualifier] ConsumeReward Int       -- Consume specified goods for reward, up to n times
-   | CConsumeThreeDifferent ConsumeReward             -- Consume 3 different kinds of goods for reward
-   | CDraw Int                                        -- Draw n cards
-   | CDrawIfLucky                                     -- Name 1-7, flip top card, keep if correct guess
-   | CDiscard ConsumeReward ConsumePowerQualifier Int -- Discard up to n cards from hand for reward
+     CPDraw Int                                        -- Draw n cards
+   | CPDrawIfLucky                                     -- Name 1-7, flip top card, keep if correct guess
+   | CPAction ConsumeAction ConsumeReward Int          -- Perform this consume action up to n times
 
-data ConsumeReward = CRVictoryPoints Int                -- Take n VPs
-                   | CRCards Int                        -- Draw n cards
-                   | CRTradePrice ConsumePowerQualifier -- Trade good for its trade price, per any qualifiers
+data ConsumeAction =
+     CAConsume [CardQualifier] -- Consume the specified cards
+   | CAConsumeThreeDifferent   -- Consume three different kinds of goods
+   | CADiscardFromHand
 
-data ConsumePowerQualifier = CPNoQualifier
-                           | CPNoTradePowers
-                           | CPNoTimesTwo
+data ConsumeReward =
+     CRVictoryPoints Int                 -- Take n VPs
+   | CRCards Int                         -- Draw n cards
+   | CRTradePrice ConsumeRewardQualifier -- Trade good for its trade price, per any qualifiers
 
+data ConsumeRewardQualifier = CPQNoQualifier
+                            | CPQNoTradePowers
+                            | CPQNoTimesTwo
+
+-- Produce --
 data ProducePower =
-     -- Base
      PDraw Int                   -- Draw n cards
    | PDrawForKind GoodKind       -- Draw 1 card for each of the specified kind of good produced
    | PDrawFor CardQualifier      -- Draw 1 card for each of the specified cards in the tableau
    | PDrawForMost GoodKind Int   -- Draw n cards if the player produced more of the specified good than any other player
-   | PDrawOnProducedWindfall Int -- Draw n cards for producing on this world
    | PDrawForDifferentKinds      -- Draw 1 card for each different kind of good produced
    | PProduceGood                -- Produce a good (if this world has no good)
-   | PProduceGoodAndDraw Int     -- Produce a good and then draw n cards (draw 0 if no good was produced)
    | PProduceWindfall GoodKind   -- Produce on a windfall of specified good
+   | PPAction ProduceAction ProduceReward
 
+data ProduceAction =
+     PAProduce CardQualifier -- Produce on the specified card
 
-data CardQualifier = NoQualifier
-                   | Not CardQualifier
-                   | AllOf [CardQualifier]
-                   | AnyOf [CardQualifier]
-                   | ThisCard
-                   | SettleQualifier SettleKind
-                   | ProductionQualifier ProductionKind
-                   | GoodQualifier GoodKind
-                   | DevelopmentQualifier DevelopmentKind
-                   | NameQualifier NameKind
+data ProduceReward =
+     PRDraw Int -- Draw n cards
+
+data CardQualifier =
+   NoQualifier
+ | Not CardQualifier
+ | AllOf [CardQualifier]
+ | AnyOf [CardQualifier]
+ | ThisCard
+ | SettleQualifier SettleKind
+ | ProductionQualifier ProductionKind
+ | GoodQualifier GoodKind
+ | DevelopmentQualifier DevelopmentKind
+ | NameQualifier NameKind
 
 data Card =
    Card { name            :: String
