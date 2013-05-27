c3a877acf20a306e8b1c8cbb6387c2e39689e096
Christopher Hebert <hebert.christopherj@gmail.com>
2013-05-27 15:54:27 -0700

    finished up the boring parts of the ui. now for the fun stuff

--- Main.hs
+++ Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Debug.Trace
+
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Gdk.GC
 import Graphics.UI.Gtk.Gdk.Drawable
@@ -34,17 +36,49 @@ infoBox = do
 
 opponentsBox :: IO HBox
 opponentsBox = do
-   box <- hBoxNew False 0
+   box  <- hBoxNew False 0
+   opp1 <- tableauBox colorRed
+   boxPackStart box opp1 PackGrow 0
    return box
+
+phaseIconImage    = imageNewFromPixbuf =<< pixbufNewFromFileAtSize "./images/no_phase_icon.jpg" 32 32
+scoreIconImage    = imageNewFromPixbuf =<< pixbufNewFromFileAtSize "./images/score_icon.jpg" 32 32
+handIconImage     = imageNewFromPixbuf =<< pixbufNewFromFileAtSize "./images/hand_icon.jpg" 32 32
+militaryIconImage = imageNewFromPixbuf =<< pixbufNewFromFileAtSize "./images/military_icon.jpg" 32 32
 
 playerStatusBox :: IO HBox
 playerStatusBox = do
-   view <- hBoxNew False 0
-   return view
-tableauBox :: IO VBox
-tableauBox = do
+   box          <- hBoxNew False 0
+   empty1       <- hBoxNew False 0
+   empty2       <- hBoxNew False 0
+   phaseIcon    <- phaseIconImage
+   scoreIcon    <- scoreIconImage
+   handIcon     <- handIconImage
+   militaryIcon <- militaryIconImage
+
+   boxPackStart box empty1       PackGrow    0
+   boxPackStart box phaseIcon    PackNatural 0
+   boxPackStart box handIcon     PackNatural 0
+   boxPackStart box scoreIcon    PackNatural 0
+   boxPackStart box militaryIcon PackNatural 0
+   boxPackStart box empty2       PackGrow    0
+   return box
+
+tableausBox :: IO VBox
+tableausBox = do
+   box       <- vBoxNew False 0
+   opponents <- opponentsBox
+   player    <- tableauBox colorBlue
+   sep       <- hSeparatorNew
+   boxPackStart box opponents PackGrow    0
+   boxPackStart box sep       PackNatural 0
+   boxPackStart box player    PackGrow    0
+   return box
+
+tableauBox :: Color -> IO VBox
+tableauBox color = do
    box          <- vBoxNew False 0
-   tableau      <- tableauDrawingArea
+   tableau      <- tableauDrawingArea color
    playerStatus <- playerStatusBox
    boxPackStart box tableau        PackGrow    0
    boxPackStart box playerStatus   PackNatural 0
@@ -96,12 +130,27 @@ actionBox = do
    boxPackStart box done    PackNatural 0
    return box
 
+cardImageDims = (372, 520)
+
+currentCardHeight :: Int -> Int
+currentCardHeight width =
+   let (cardWidth, cardHeight) = cardImageDims
+       ratio = (fromIntegral cardHeight) / (fromIntegral cardWidth)
+       height = fromIntegral . round $ (fromIntegral width / 6.0) * ratio
+   in height
+
+currentHandHeight :: Int -> Int
+currentHandHeight width =
+   let height = currentCardHeight width
+       padding = height `quot` 10
+   in height + padding
+
 drawCurrentHand :: DrawingArea -> Event -> IO Bool
 drawCurrentHand drawingArea _ = do
+   (width, _) <- widgetGetSize drawingArea
    drawWindow      <- widgetGetDrawWindow drawingArea
-   (width, height) <- widgetGetSize drawingArea
    gc              <- gcNew drawWindow
-   pix             <- pixbufNewFromFileAtSize "./images/card_back.jpg" (width `quot` 6) height
+   pix             <- pixbufNewFromFileAtSize "./images/card_back.jpg" (width `quot` 6) (-1)
    drawPixbuf drawWindow gc pix 0 0 0 0 (-1) (-1) RgbDitherNone 0 0
    return True
 
@@ -110,50 +159,54 @@ drawCurrentTableau drawingArea _ = do
    drawWindow      <- widgetGetDrawWindow drawingArea
    (width, height) <- widgetGetSize drawingArea
    gc              <- gcNew drawWindow
-   pix             <- pixbufNewFromFileAtSize "./images/card_back.jpg" (width `quot` 6) height
+   pix             <- pixbufNewFromFileAtSize "./images/card_back.jpg" (width `quot` 6) (-1)
    drawPixbuf drawWindow gc pix 0 0 0 0 (-1) (-1) RgbDitherNone 0 0
    return True
+
+requestHandDrawingAreaSize drawingArea _ = do
+   (width, height) <- widgetGetSize drawingArea
+   widgetSetSizeRequest drawingArea (-1) (currentHandHeight width)
 
 -- The Drawing Area for the player's current hand.
 handDrawingArea ::  IO DrawingArea
 handDrawingArea = do
    drawingArea <- drawingAreaNew
+   onSizeAllocate drawingArea (requestHandDrawingAreaSize drawingArea)
    onExpose drawingArea (drawCurrentHand drawingArea)
-   widgetQueueDraw drawingArea
    return drawingArea
 
-tableauDrawingArea :: IO DrawingArea
-tableauDrawingArea = do
+colorBlue = Color 0x9000 0xa700 0xca00
+colorRed  = Color 0xe700 0x6d00 0x6400
+
+tableauDrawingArea :: Color -> IO DrawingArea
+tableauDrawingArea color = do
    drawingArea <- drawingAreaNew
+   widgetModifyBg drawingArea StateNormal color
    onExpose drawingArea (drawCurrentTableau drawingArea)
-   widgetQueueDraw drawingArea
    return drawingArea
 
 playerBox = do
    box     <- vBoxNew False 0
-   tableau <- tableauBox
    phase   <- phaseBox
    action  <- actionBox
    hand    <- handDrawingArea
    sep1    <- hSeparatorNew
    sep2    <- hSeparatorNew
-   sep3    <- hSeparatorNew
-   boxPackStart box tableau PackGrow    0
-   boxPackStart box sep1    PackNatural 0
    boxPackStart box phase   PackNatural 0
-   boxPackStart box sep2    PackNatural 0
+   boxPackStart box sep1    PackNatural 0
    boxPackStart box action  PackNatural 0
-   boxPackStart box sep3    PackNatural 0
+   boxPackStart box sep2    PackNatural 0
    boxPackStart box hand    PackNatural 0
    return box
 
 gamePlayBox :: IO VBox
 gamePlayBox = do
    box       <- vBoxNew False 0
-   opponents <- opponentsBox
+   tableaus  <- tableausBox
    player    <- playerBox
-   boxPackStart box opponents PackNatural 0
-   boxPackStart box player    PackGrow    0
+   sep       <- hSeparatorNew
+   boxPackStart box tableaus PackGrow    0
+   boxPackStart box player   PackNatural 0
    return box
 
 menuBar :: IO MenuBar
